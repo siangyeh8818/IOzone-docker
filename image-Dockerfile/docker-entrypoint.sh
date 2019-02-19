0b81094b949f61a5866d08ef10e1b65af3197f4f
siangyeh8818 <siangyeh8818@gmail.com>
2019-02-19 14:23:59 +0800

    fix docker-entrypoint.sh

--- image-Dockerfile/docker-entrypoint.sh
+++ image-Dockerfile/docker-entrypoint.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
-echo $TestModel
-echo $Concurrency
-echo $FileSize
-echo $RecordSize
+
+echo "測試過程檔案寫入大小 : $FileSize "
+echo "測試過程寫入紀錄大小 : $RecordSize "
+
 
 if [ "$TestModel" = "auto-mode" ];then
    echo "自動全面測試, iozone測試模式選用'0=write/rewrite , 1=read/re-read'"
-   echo "最大檔案大小 : $MaxFileSize"
-   echo "最小檔案大小: $MinFileSize"
    echo "測試過程檔案寫入位址 : /mnt/"
    ./iozone -a -s $FileSize -r $RecordSize -i 0 -i 1  -f /mnt/iozone -Rb ./iozone.xls
 elif [ "$TestModel" = "throughput" ];then
    echo "吞吐量測試, iozone測試模式選用'0=write/rewrite , 1=read/re-read'"
    echo "並發數 ：$Concurrency"
-   echo "最大檔案大小 : $MaxFileSize"
-   echo "最小檔案大小: $MinFileSize"
    ./iozone -s $FileSize -r $RecordSize -i 0 -i 1 -t $Concurrency -r 1m -G -o -B  -f /mnt/iozone -Rb ./iozone.xls
 else
    echo "can't identify the value of TestModel (env)"
    echo "load default model ........" 
    echo "自動全面測試, iozone測試模式選用'0=write/rewrite , 1=read/re-read'"
-   echo "最大檔案大小 : $MaxFileSize"
-   echo "最小檔案大小: $MinFileSize"
    echo "測試過程檔案寫入位址 : /mnt/"
    ./iozone -a -s $FileSize -r $RecordSize -i 0 -i 1  -f /mnt/iozone -Rb ./iozone.xls
 fi
