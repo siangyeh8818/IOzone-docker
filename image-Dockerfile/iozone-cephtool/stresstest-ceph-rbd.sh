0dbf06b44f30207be42432ef957be9112493f876
siangyeh8818 <siangyeh8818@gmail.com>
2019-02-25 15:44:47 +0800

    add iozone-cephtool/*

---
+++ image-Dockerfile/iozone-cephtool/stresstest-ceph-rbd.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo "測試過程檔案寫入大小 : $FileSize "
+echo "測試過程寫入紀錄大小 : $RecordSize "
+echo "壓測過程中刪除的pool名稱 : $PoolName "
+
+if [ "$TestModel" = "auto-mode" ];then
+   echo "自動全面測試, iozone測試模式選用'0=write/rewrite , 1=read/re-read'"
+   echo "測試過程檔案寫入位址 : /mnt/"
+   set j=2
+   while true
+   do
+       ./iozone -a -s $FileSize -r $RecordSize -i 0 -i 1  -f /mnt/iozone -Rb ./iozone.xls
+       echo "delete object from poll ( $PoolName )................."
+       for i in `rados -p $PoolName ls`;
+       do
+           echo $i; rados -p $PoolName rm $i;
+       done
+       echo "Finishing to delete object from poll ( $PoolName )"
+   done
+elif [ "$TestModel" = "throughput" ];then
+   echo "吞吐量測試, iozone測試模式選用'0=write/rewrite , 1=read/re-read'"
+   echo "並發數 ：$Concurrency"
+   cp iozone /mnt
+   chmod +x /mnt/iozone
+   set j=2
+   while true
+   do
+       bash -c "/mnt/iozone -s $FileSize -r $RecordSize -i 0 -i 1 -t $Concurrency  -Rb ./iozone.xls"
+       echo "delete object from poll ( $PoolName )................."
+       for i in `rados -p $PoolName ls`;
+       do
+           echo $i; rados -p $PoolName rm $i;
+       done
+       echo "Finishing to delete object from poll ( $PoolName )"
+   done
+else
+   echo "can't identify the value of TestModel (env)"
+   echo "load default model ........" 
+   echo "自動全面測試, iozone測試模式選用'0=write/rewrite , 1=read/re-read'"
+   echo "測試過程檔案寫入位址 : /mnt/"
+   set j=2
+   while true
+   do
+       ./iozone -a -s $FileSize -r $RecordSize -i 0 -i 1  -f /mnt/iozone -Rb ./iozone.xls
+       echo "delete object from poll ( $PoolName )................."
+       for i in `rados -p $PoolName ls`;
+       do
+           echo $i; rados -p $PoolName rm $i;
+       done
+       echo "Finishing to delete object from poll ( $PoolName )"
+   done
+fi
+
+sleep inf
---
+++ image-Dockerfile/iozone-cephtool/toolbox.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -e
+
+ARGS="$@"
+if [ $# -eq 0 ]; then
+    ARGS=/bin/bash
+fi
+
+ROOK_DIR="/var/lib/rook"
+CEPH_CONFIG="/etc/ceph/ceph.conf"
+MON_CONFIG="/etc/rook/mon-endpoints"
+KEYRING_FILE="/etc/ceph/keyring"
+
+# create a ceph config file in its default location so ceph/rados tools can be used
+# without specifying any arguments
+write_endpoints() {
+    endpoints=$(cat ${MON_CONFIG})
+
+    # filter out the mon names
+    mon_endpoints=$(echo ${endpoints} | sed 's/[a-z]\+=//g')
+
+    # filter out the legacy mon names
+    mon_endpoints=$(echo ${mon_endpoints} | sed 's/rook-ceph-mon[0-9]\+=//g')
+
+    DATE=$(date)
+    echo "$DATE writing mon endpoints to ${CEPH_CONFIG}: ${endpoints}"
+    cat <<EOF > ${CEPH_CONFIG}
+[global]
+mon_host = ${mon_endpoints}
+
+[client.admin]
+keyring = ${KEYRING_FILE}
+EOF
+}
+
+# watch the endpoints config file and update if the mon endpoints ever change
+watch_endpoints() {
+    # get the timestamp for the target of the soft link
+    real_path=$(realpath ${MON_CONFIG})
+    initial_time=$(stat -c %Z ${real_path})
+    while true; do
+       real_path=$(realpath ${MON_CONFIG})
+       latest_time=$(stat -c %Z ${real_path})
+
+       if [[ "${latest_time}" != "${initial_time}" ]]; then
+         write_endpoints
+         initial_time=${latest_time}
+       fi
+       sleep 10
+    done
+}
+
+# create the keyring file
+cat <<EOF > ${KEYRING_FILE}
+[client.admin]
+key = ${ROOK_ADMIN_SECRET}
+EOF
+
+# write the initial config file
+write_endpoints
+
+# continuously update the mon endpoints if they fail over
+watch_endpoints
+echo "Start to 壓力測試對rbd.........."
+sleep 2s
+/bin/bash -c /iozone3_327/src/current/stresstest-ceph-rbd.sh
